f5baa47d7158984cc0eabcccfab2e77b2ffb7a41
bttqjs <167028866+bttqjs@users.noreply.github.com>
2025-06-19 09:01:18 +0200

    11

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  output: 'export',
-  basePath: '', // Empty for username.github.io repos
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  trailingSlash: true, // Helps with GitHub Pages routing
-}
